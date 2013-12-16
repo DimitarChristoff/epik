581c8a2369a9b8230d255ed5b54ed888b48ef33c
DimitarChristoff <christoff@gmail.com>
2013-12-16 10:36:13 +0000

    all v1 func in model

--- example/model.js
+++ example/model.js
@@ -3,7 +3,28 @@ define(['../lib/model'], function(model){
 
 	var m = new model({
 		one: 1
+	}, {
+		defualts: {
+			name: 'Robert'
+		},
+		onChange: function(keys){
+			console.log(keys);
+		},
+		'onChange:one': function(value){
+			console.log('one changed to ' + value);
+		},
+		onEmpty: function(){
+			console.log('emptied', this.toJSON());
+		}
 	});
 
 	console.log(m.toJSON());
+
+	m.set({
+		one: m.get('one') + 1,
+		name: 'Bob'
+	});
+
+	m.set('name', 'Bobby');
+	m.empty();
 });
