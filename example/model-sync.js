85e094f82edf98fb6019bf884e422080ea418830
DimitarChristoff <christoff@gmail.com>
2013-12-28 19:49:54 +0000

    somewhat working model.fetch

---
+++ example/model-sync.js
@@ -0,0 +1,90 @@
+require.config({
+
+	baseUrl: '../',
+
+	paths: {
+		primish: 'lib/components/primish',
+		lodash: 'lib/components/lodash/dist/lodash'
+	}
+});
+
+define(['lib/index', 'lib/model-sync'], function(epic, model){
+
+	var	primish = epic.primish,
+		_ = epic._;
+
+	var Person = primish({
+
+		extend: model,
+
+		options: {
+			defaults: {
+				name: 'Robert',
+				surname: 'Roberts',
+				age: 30
+			}
+		},
+
+		validators: {
+			age: function(value){
+				return parseInt(value, 10) == value ? true : 'Age needs to be an integer';
+			},
+			name: function(value){
+				return (value.charAt(0).toLowerCase() !== value.charAt(0)) || 'Name needs to be capitalized';
+			}
+		}
+
+	});
+
+	var bob = new Person({
+		job: 'accountant',
+	}, {
+		onChange: function(keys){
+			console.log(keys);
+		},
+		'onChange:one': function(value){
+			console.log('one changed to ' + value);
+		},
+		onEmpty: function(){
+			console.log('emptied', this.toJSON());
+		},
+		onError: function(errors){
+			console.warn('WARNING: validation errors have occurred');
+			_.forEach(errors, function(error){
+				console.log('rejected ' + error.key + ' => ' + error.value + ' hint: ' + error.error);
+			});
+		},
+		onFetch: function(){
+			console.log('fetched');
+		},
+		onFailure: function(response){
+			console.log('sync error occurred', response.status);
+		}
+	});
+
+	console.log(bob.get('id'));
+	console.log(bob.toJSON());
+
+	bob.set({
+		age: bob.get('age') + 1,
+		name: 'Bob'
+	});
+
+	bob.set('name', 'Bobby');
+
+	console.log(bob.get(['name', 'surname', 'age']));
+
+	bob.set({
+		age: 31.5,
+		name: 'bob'
+	});
+	bob.empty();
+
+	bob.set({
+		id: '1.json',
+		urlRoot: 'api/users'
+	});
+
+	bob.fetch();
+
+});
