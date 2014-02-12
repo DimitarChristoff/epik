7eaf91a324824f50bff4957659a0484b0cdcb94b
DimitarChristoff <christoff@gmail.com>
2014-02-12 15:55:49 +0000

    fixed failing nodejs group

--- test/buster.js
+++ test/buster.js
@@ -44,31 +44,3 @@ config['Epik browser tests'] = {
 	]
 };
 
-
-// tests disabled as buster-test with both groups right now does not proc.exit
-config['Node tests'] = {
-	rootPath: '../',
-
-	environment: 'node',
-
-	libs: [
-		'lib/components/lodash/dist/lodash.js',
-		'lib/components/primish/primish.js',
-		'lib/components/primish/emitter.js',
-		'lib/components/primish/options.js',
-		'lib/components/slicker/index.js',
-		'lib/index.js'
-	],
-
-	sources: [
-		//'lib/index.js',
-		'lib/model.js',
-		'lib/collection.js'
-	],
-
-	tests: [
-		// find matching test specs as above sources
-		'test/specs/model-test.js',
-		'test/specs/collection-test.js'
-	]
-};
